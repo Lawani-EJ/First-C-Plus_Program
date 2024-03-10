8cc8231a293f2ceb5988005bdd1e9c5dcd32f72d
elyonjohn <elyonjohn60@gmail.com>
2024-03-11 00:01:52 +0100

    Created C++

---
+++ second_sem.cpp
@@ -0,0 +1,9 @@
+// Lawani elyon john first c++ program
+// Time: 2:26
+// Date: 6/9/2021
+#include<iostream>
+int main()
+{
+	std:: cout <<"I elyon john lawani am here again. \n welcome to the 2nd semester! \n";	
+	std:: cout << "peace love and positivity";
+}
